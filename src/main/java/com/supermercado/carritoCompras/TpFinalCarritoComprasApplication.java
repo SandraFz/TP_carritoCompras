40d2c2e710fef6a95fefae33d7b87ecae3306b21
Sandra <sandranfernandez@gmail.com>
2023-05-29 20:14:18 -0300

    First commit.

---
+++ src/main/java/com/supermercado/carritoCompras/TpFinalCarritoComprasApplication.java
@@ -0,0 +1,13 @@
+package com.supermercado.carritoCompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalCarritoComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalCarritoComprasApplication.class, args);
+	}
+
+}
